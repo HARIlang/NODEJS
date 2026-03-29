ac699df07c91a4a88f85846d8744d0d99ce06c50
Hari <amarhari26122002@gmail.com>
2026-03-29 19:17:55 +0530

    exported and imported the module in the base module

--- add.js
+++ add.js
@@ -1,12 +1,14 @@
-const sum = (a,b,c) =>{
-return a+b+c;
-}
+const add = () => {
+  const sum = (a, b, c) => {
+    return a + b + c;
+  };
 
+  const randomNum = Math.floor(Math.random() * 100) + 1;
+  const a = randomNum;
+  const b = randomNum;
+  const c = randomNum;
 
-const randomNum = Math.floor(Math.random()*100)+1;
-const a = randomNum;
-const b = randomNum;
-const c = randomNum;
+  console.log(sum(a, b, c));
+};
 
-const add = sum(a,b,c);
-console.log(add);
+module.exports = add
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-require('./add.js');
+const add = require('./add.js');
+
 console.log('hello-world');
+add();
