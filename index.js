cdef81e02d19db6ea9259c7e7a988483ed96ee07
Hari <amarhari26122002@gmail.com>
2026-04-02 19:24:25 +0530

    fs sync and async

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-const buffer = new Buffer.from("hari");
+const { error } = require('console');
+const fs = require('fs');
 
-console.log(buffer);
-console.log(buffer.toJSON());
+
+const filecontent = fs.readFileSync('./sample.txt','utf-8');
+console.log(filecontent);
+
+fs.readFile('./sample.txt','utf-8' , (error,data)=>{    // best method while it comes to multiple users
+  if(error){
+  console.log('the error is ' +  error)
+  }
+  else{
+
+    console.log('this is from async callback function'+data);
+  }
+});
+
+
+// writing file with sync method;
+
+fs.writeFileSync('syncwrite.txt','this is written by sync method');
+
+
+// writing file with async method 
+
+
+fs.writeFile('async.txt','this written by async method',(err)=>{
+
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log('the file written')
+    }
+});
+
+
+
